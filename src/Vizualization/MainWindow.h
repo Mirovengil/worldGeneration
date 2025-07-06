1d9b21999429bb58c99621e811e0b67f5ff35525
Mirovengil <Mirivengil@gmail.com>
2025-07-06 15:16:31 +0300

    Оформил базовую структуру проекта.

---
+++ src/Vizualization/MainWindow.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_WINDOW_H
+#define MAIN_WINDOW_H
+
+#include <QMainWindow>
+#include <QWidget>
+
+class MainWindow : public QMainWindow
+{
+Q_OBJECT
+
+public:
+    MainWindow();
+    ~MainWindow();
+private:
+};
+
+#endif
